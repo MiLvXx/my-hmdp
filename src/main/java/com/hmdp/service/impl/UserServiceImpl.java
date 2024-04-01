4f7e39fd1c2fe9932d531f0589b4d35c2e02afac
xuxin <2860543348@qq.com>
2024-04-01 19:28:24 +0800

    [feature] Add Send Code Feature

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,15 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -17,4 +22,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 1.校验手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            // 2.如果不符合，返回错误信息
+            return Result.fail("手机号格式错误");
+        }
+        // 3.符合，生成验证码(6位)
+        String code = RandomUtil.randomNumbers(6);
+        // 4.保存验证码到 session
+        session.setAttribute("code", code);
+        // 5.发送验证码
+        String message = "发送短信验证成功, 验证码: " + code;
+        log.debug(message);
+
+        return Result.ok();
+    }
 }
